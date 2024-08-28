5e995cd8b9c7f9525af2b04e727ff512c98d9aaf
Limazxz <goubulsegue@gmail.com>
2024-08-28 11:01:06 -0300

    inserindo bootstrap

--- AplicacaoMVCLinq/Controllers/ClienteController.cs
+++ AplicacaoMVCLinq/Controllers/ClienteController.cs
@@ -11,7 +11,7 @@ namespace AplicacaoMVCLinq.Controllers
         }
         public IActionResult GetCliente()
         {
-            Cliente c1 = new Cliente(1, "Pedro Lima", "silvaelima@gmail.com", "1499662", new DateOnly(2000, 3, 17));
+            Cliente c1 = new Cliente(1, "Pedro Lima ", "silvaelima@gmail.com", "1499662", new DateOnly(2000, 3, 17));
             Cliente c2 = new Cliente(2, "Mike", "mikeon@gmail.com", "1499661", new DateOnly(2001, 3, 21));
             Cliente c3 = new Cliente(3, "Jhonatan", "jhontan34@gmail.com", "1499664", new DateOnly(2002, 3, 20));
             Cliente c4 = new Cliente(4, "Steven", "stven675@gmail.com", "1499663", new DateOnly(2003, 8, 11));
